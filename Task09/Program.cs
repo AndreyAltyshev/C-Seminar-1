020a914837627852eba9115c61043fef4a8780a0
Norros <Andriyxa_77@mail.ru>
2023-02-14 20:43:50 +0300

    Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -4,9 +4,24 @@
 78 -> 8
 12-> 2
 85 -> 8 */
+
+/*Без Методов 
+
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 int firstDigit = number / 10;
 int secondGigit = number % 10;
 if (firstDigit > secondGigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа => {secondGigit}");
+else Console.WriteLine($"Наибольшая цифра числа => {secondGigit}"); */
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+int MaxDigit(int num)
+{
+    int firstDigit = number / 10;
+    int secondGigit = number % 10;
+    if (firstDigit > secondGigit) return firstDigit;
+    else return secondGigit;
+}
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
