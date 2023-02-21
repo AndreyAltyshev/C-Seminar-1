6ef7f4cd149772efbc663a656e220c785b0c3619
Norros <Andriyxa_77@mail.ru>
2023-02-21 19:59:39 +0300

    sem_3

---
+++ Task17/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка. */
+
+//методы
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int Quartyr(int x , int y)
+{
+    if(x>0 && y>0) return 1;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y<0) return 3;
+    if(x>0 && y<0) return 4;
+    return 0;
+}
+
+//тело программы
+int xc = Prompt("Введите координату X ");
+int yc = Prompt("Введите координату Y ");
+int quartyr = Quartyr(xc, yc);
+string result = quartyr>0 ? 
+                $"Указанные координаты соответствют четверти {quartyr}" 
+                : "Введены не верные координаты"; 
+Console.WriteLine(result);
---
+++ Task22/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+//тело программы
+int number = Prompt("Введите число N ");
+if (number > 0) SquarN(number);
+else Console.WriteLine("введено не верное значение");
+
+//методы
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void SquarN(int k)
+{
+    for (int i = 1; i <= k; i++)
+    {
+        Console.WriteLine($"{i}      {Math.Pow(i, 2)}");
+    }
+    return;
+}
